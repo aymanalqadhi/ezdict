cef87643e562219a1ccd0fe4bce254b70210153c
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2019-05-20 10:24:32 +0300

    Added initial files

---
+++ ezdict/ezdict/command/command.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+class Command():
+    """
+    A Class to represent a command executed with passed command line arguments
+    list
+    """ 
+    def __init__(self, x: list):
+        for arg in args:
+            print(arg)
